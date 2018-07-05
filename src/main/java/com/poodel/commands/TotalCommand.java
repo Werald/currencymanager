5e43a9a7039b0a7dfd2d041ecc4b82643182fe2c
werald_che <chealexpud@gmail.com>
2018-07-05 19:27:25 +0300

    clear, add, help => working perfect list => bug with output (not grouped) total => working allmost perfect(1 exception hidden) { getCurrencys working perfect (counting sum amount of each currency in table)

--- src/main/java/com/poodel/commands/TotalCommand.java
+++ src/main/java/com/poodel/commands/TotalCommand.java
@@ -1,14 +1,34 @@
 package com.poodel.commands;
 
+import com.poodel.database_manager.TableTotal;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class TotalCommand extends AbsCommands {
     private static final String COMMAND_PARSER = "[t][o][t][a][l]\\s[A-Z]{3}";
     private static final String ERROR_MESSAGE = "No expenses found.";
 
     public void execute(String inCommand) {
+        String [] parsedCommand = getDataArrFromUI(inCommand);
+        try {
 
+        if(checkCommand(inCommand, COMMAND_PARSER)) {
+            TableTotal tableTotal = new TableTotal();
+            DecimalFormat df = new DecimalFormat("#.##");
+            df.setRoundingMode(RoundingMode.CEILING);
+            double result = tableTotal.getTotal(parsedCommand[1]);
+
+            System.out.println(df.format(result));
+
+        }
+
+        } catch (Exception e){
+            System.out.println(e.getClass().getName() + "; " + e.getMessage());
+        }
     }
 
     public String[] getDataArrFromUI(String inCommand) {
-        return new String[0];
+        return inCommand.split("\\s", 2);
     }
 }
--- src/main/java/com/poodel/database_manager/TableTotal.java
+++ src/main/java/com/poodel/database_manager/TableTotal.java
@@ -1,6 +1,9 @@
 package com.poodel.database_manager;
 
+import com.poodel.fixer_resourses.CurrencyType;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONString;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -11,25 +14,97 @@ import java.util.*;
 
 public class TableTotal {
 
-    private static final String URL_TO_SEND = "http://data.fixer.io/api/latest?access_key=1787cfc17beaea6bf7ba65cb4a26aebe&symbols=USD,AUD,CAD,PLN,MXN&format=1";
-    private String connectToUrl(String sds){
+    private static final String URL_TO_SEND = "http://data.fixer.io/api/latest?access_key=1787cfc17beaea6bf7ba65cb4a26aebe";
+
+    private HashMap<String, Double> getRequiredCoursesFromFixer(String CUR){
+        ArrayList<String> currencys = new ArrayList<>(getAbreviatures());
+        HashMap<String, Double> jsonCur = new HashMap<>();
+
+
+//        System.out.println(currencys.get(0));
         try
         {
-            
+            URL url = new URL(URL_TO_SEND);
+            // read from the URL
+            Scanner scan = new Scanner(url.openStream());
+            String str = "";
+            while (scan.hasNext())
+                str += scan.nextLine();
+            scan.close();
+            // build a JSON object
+            JSONObject obj = new JSONObject(str);
+            // get the result
+            JSONObject res = obj.getJSONObject("rates");
+            for (String currency : currencys) {
+                jsonCur.put(currency, new Double(String.valueOf(res.getDouble(currency))));
+            }
+            jsonCur.put(CUR, new Double(String.valueOf(res.getDouble(CUR))));
+
+//            System.out.println(res);
+
         }
         catch(Exception e)
         {e.printStackTrace();}
-        return null;
+        return jsonCur;
+
     }
-//
-//    public static void main (String S[]){
-//        TableTotal tableTotal = new TableTotal();
-//        tableTotal.connectToUrl(URL_TO_SEND);
-//    }
-//
+
+    public Double getTotal(String cur) {
+        Double result = 0.0;
+        ArrayList<String> currencys = new ArrayList<>(getAbreviatures());
+        currencys.add(cur);
+        HashMap<String, Double> jsonCur = new HashMap<>();
+        HashMap<String, Double> tableCur = new HashMap<>();
+        tableCur = getCurrencys();
+
+        jsonCur = getRequiredCoursesFromFixer(cur);
+//        System.out.println(currencys);
+        double curCourse = jsonCur.get(cur);
+        try {
+
+            for (String val : currencys
+                    ) {
+                double temp = tableCur.get(val) * (curCourse / jsonCur.get(val));
+//                System.out.println(temp);
+                result += temp;
+            }
+
+        } catch (NullPointerException e){
+        }
+        return result;
+    }
 
 //    [PLN, USD]
 //    {PLN=27.0, USD=1526.0}
+    private HashSet<String> getAbreviatures(){
+        HashSet<String> currencys = new HashSet<>();
+        HashMap<String, Double> curAmount = new HashMap<>();
+
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:expenses1.db");
+            c.setAutoCommit(false);
+//            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT CURRENCY FROM EXPENSES; ");
+
+            while (rs.next()) {
+                currencys.add(rs.getString("CURRENCY"));
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+
+        return currencys;
+
+        }
     private HashMap<String, Double> getCurrencys(){
         HashSet<String> currencys = new HashSet<>();
         HashMap<String, Double> curAmount = new HashMap<>();
@@ -40,7 +115,7 @@ public class TableTotal {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:expenses1.db");
             c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+//            System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT CURRENCY FROM EXPENSES; ");
@@ -59,7 +134,7 @@ public class TableTotal {
                 }
                 result.close();
             }
-            System.out.println(curAmount.toString());
+//            System.out.println(curAmount.toString());
             stmt.close();
             c.close();
         } catch ( Exception e ) {
@@ -69,47 +144,5 @@ public class TableTotal {
         return curAmount;
     }
 
-    private String [] getAmount(HashSet<String> currencys){
-        String[] result = null;
 
-
-        return result;
-    }
-    public static void main(String args[]) throws Exception {
-
-        TableTotal tableTotal = new TableTotal();
-
-        System.out.println(tableTotal.getCurrencys().toString());
-
-        geocoding("Lviv");
-    }
-
-    public static void geocoding(String addr) throws Exception
-    {
-        // build a URL
-        String s = "http://maps.google.com/maps/api/geocode/json?" +
-                "sensor=false&address=";
-        s += URLEncoder.encode(addr, "UTF-8");
-        URL url = new URL(s);
-
-        // read from the URL
-        Scanner scan = new Scanner(url.openStream());
-        String str = new String();
-        while (scan.hasNext())
-            str += scan.nextLine();
-        scan.close();
-
-        // build a JSON object
-        JSONObject obj = new JSONObject(str);
-        if (! obj.getString("status").equals("OK"))
-            return;
-
-        // get the first result
-        JSONObject res = obj.getJSONArray("results").getJSONObject(0);
-        System.out.println(res.getString("formatted_address"));
-        JSONObject loc =
-                res.getJSONObject("geometry").getJSONObject("location");
-        System.out.println("lat: " + loc.getDouble("lat") +
-                ", lng: " + loc.getDouble("lng"));
-    }
 }
